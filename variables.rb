c5973b7abb77f547fa072c7d330caef311d7d2c3
Davis Weimer <davisweimer@gmail.com>
2023-03-27 19:16:01 -0400

    Add puts & var changes

--- variables.rb
+++ variables.rb
@@ -1,16 +1,42 @@
 name = "Davis"
+puts name
+name = "Daisy"
+puts name
+
 favorite_color = "Camo"
+puts favorite_color
+favorite_color = "Green"
+puts favorite_color
+
 favorite_food = "Pizza"
+puts favorite_food
+favorite_food = "Falafel"
+puts favorite_food
+ 
 
 steps_taken = 23445
+puts steps_taken
+steps_taken = 46298
+puts steps_taken
+
 turing_cost = 45000
 hours_slept = 6
 stress_level = 5
 tolerable_stress_level = 8
 
 funny_number = 420.69
+puts funny_number
+funny_number = 1234567890
+puts funny_number
+
 mariana_Trench_depth_meters = 11033.61
 
+
 email_address_valid = false
+puts email_address_valid
+email_address_valid = true
+puts email_address_valid
+
 password_length = true
+
 
